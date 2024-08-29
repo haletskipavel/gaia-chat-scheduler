f5e8bf4214f7878850276d5641c36d3f47dd3ec5
Pavel Haletski <haletski.p@gmail.com>
2024-08-29 16:33:02 +0400

    git commit

--- Airdrops.GaiaChat.Scheduler/Infrastructure/InfrastructureDependencies.cs
+++ Airdrops.GaiaChat.Scheduler/Infrastructure/InfrastructureDependencies.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Polly.Extensions.Http;
+﻿using Polly.Extensions.Http;
 using Polly;
 using Refit;
 using Airdrops.Nodes.Infrastructure.Abstractions;
-using Microsoft.Extensions.Configuration;
+using Serilog;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Airdrops.Nodes.Infrastructure
 {
@@ -21,8 +22,9 @@ namespace Airdrops.Nodes.Infrastructure
                     ArgumentException.ThrowIfNullOrWhiteSpace(opentDbBaseUrl, nameof(opentDbBaseUrl));
 
                     c.BaseAddress = new Uri(opentDbBaseUrl);
+                    c.Timeout = TimeSpan.FromMinutes(2);
                 })
-                .AddPolicyHandler(_ => GetPolicy());
+                .AddPolicyHandler(GetPolicy);
 
             services
                .AddRefitClient<IGaiaChatApiClient>()
@@ -33,17 +35,23 @@ namespace Airdrops.Nodes.Infrastructure
 
                    ArgumentException.ThrowIfNullOrWhiteSpace(gaiaChatBaseUrl, nameof(gaiaChatBaseUrl));
                    c.BaseAddress = new Uri(gaiaChatBaseUrl);
+                   c.Timeout = TimeSpan.FromMinutes(2);
                })
-               .AddPolicyHandler(_ => GetPolicy());
+               .AddPolicyHandler(GetPolicy);
 
             return services;
 
-            static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+            static IAsyncPolicy<HttpResponseMessage> GetPolicy(IServiceProvider serviceProvider, HttpRequestMessage requestMessage)
             {
+                var logger = serviceProvider.GetRequiredService<ILogger>();
                 return HttpPolicyExtensions
                     .HandleTransientHttpError()
                     .OrResult(response => (int)response.StatusCode == 429 || (int)response.StatusCode > 500)
-                    .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+                    .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), 
+                    (outcome, timespan, retryAttempt, context) =>
+                    {
+                        logger.LogWarning("Retry {RetryAttempt} encountered an error. Waiting {Timespan} before next retry. Outcome: {StatusCode}", retryAttempt, timespan, outcome.Result.StatusCode);
+                    });
             }
         }
     }
