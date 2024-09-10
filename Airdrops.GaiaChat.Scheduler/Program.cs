64600a84fa0077f7c85ac4042a705895451f9cee
Pavel Haletski <haletski.p@gmail.com>
2024-09-10 13:03:19 +0400

    update time

--- Airdrops.GaiaChat.Scheduler/Program.cs
+++ Airdrops.GaiaChat.Scheduler/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddQuartz(q =>
         .WithIdentity(nameof(GaiaChatJob))
         .StartNow()
         .WithSimpleSchedule(x => x
-            .WithInterval(TimeSpan.FromMinutes(new Random().Next(1, 3)))
+            .WithInterval(TimeSpan.FromMinutes(new Random().Next(1, 2)))
             .RepeatForever())
     );
 });
